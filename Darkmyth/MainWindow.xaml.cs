b376dfedb196dd7d1e1f7df956d22421fba7b7d9
tpclumibao <tcl2245@students.uc-bcf.edu.ph>
2024-03-04 20:10:44 +0800

    view display info

---
+++ Darkmyth/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Engine.Models.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CC31B_PowerpuffBoys;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private GameSession _gameSession;
+    public MainWindow()
+    {
+        InitializeComponent();
+        _gameSession = new GameSession();
+        DataContext = _gameSession;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        _gameSession.CurrentPlayer.Gold += 10;
+    }
+}
---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Engine.Models.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public GameSession() 
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Themis";
+            CurrentPlayer.CharacterClass = "Inquisitor";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
