94d5b886203ae541e5eb3d00693257929ff3dee1
tpclumibao <tcl2245@students.uc-bcf.edu.ph>
2024-04-16 21:21:01 +0800

    ewn

--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a663b6019a9459f8fd6cb53f5e8f4758625ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54635397da8505a2399f00dcb9995b7500ddb6eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
